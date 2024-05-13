d37d48fdd0212815245660434b23e93d82547628
joaquinlapiere <80705785+joaquinlapiere@users.noreply.github.com>
2024-05-13 15:35:43 +0200

    eerste meeting qwiicscale testen

--- Python/Brug IOT/sensordata/qwiic.py
+++ Python/Brug IOT/sensordata/qwiic.py
@@ -1,6 +1,13 @@
 from libraries import qwiicscale
+#import datetime
 
 qwiic = qwiicscale()
 qwiic.begin()
 
-sensorwaarde = qwiic.getReading()
+while True:
+    sensorwaarde = qwiic.getReading()
+
+    #raw_time = datetime.datetime.now()
+    #tijd = str(raw_time.hour) + ":" + str(raw_time.minute) + ":" + str(raw_time.second)
+
+    print(sensorwaarde)
