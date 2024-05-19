56cfb2bc1d0cb721bfe19854c96bb510c48f8eca
joaquinlapiere <80705785+joaquinlapiere@users.noreply.github.com>
2024-05-19 19:04:43 +0200

    nieuw bestand met code om gedrag rekstrookje en qwiic te testen -

---
+++ Python/test_qwiic. functies.py
@@ -0,0 +1,37 @@
+import qwiicscale
+import datetime
+
+Average_Amount: int = 100
+Aantal_Iteraties: int = int(input('hoeveel iteraties?'))
+
+scale = qwiicscale.QwiicScale()
+
+while True:
+    optie: int = int(input("welke optie? [1] rauwe sensordata, [2]rauwe sensordata + tijd, [3] Nulwaarde"))
+    if optie == 1:
+        i: int = 0
+        for i in range(0, Aantal_Iteraties):
+            i = i + 1
+            sensorwaarde = scale.getReading()
+            print(sensorwaarde)
+
+    elif optie == 2:
+        i: int = 0
+        for i in range(0, Aantal_Iteraties):
+            i = i + 1
+            sensorwaarde = scale.getReading()
+            raw_time = datetime.datetime.now()
+            tijd = str(raw_time.hour) + ":" + str(raw_time.minute) + ":" + str(raw_time.second)
+            print(sensorwaarde, tijd)
+
+    elif optie == 3:
+        i: int = 0
+        print("plaats geen gewicht en duw op enter")
+        input()
+        Nulwaarde: int = scale.getAverage(Average_Amount)
+
+        print("nulwaarde gemeten: druk enter")
+        input()
+        for i in range(0, Aantal_Iteraties):
+            waarde: int = scale.getReading() - Nulwaarde
+            print(Nulwaarde)
