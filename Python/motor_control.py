c05afae696a11540c00d24e712a9a42e9d16ab8c
Arthur Smalle <arthur@smalle.be>
2024-05-22 00:50:08 +0200

    todo voor laatste aanpassingen toegevoegd timestamp en clear toegevoegd in qwiic clear url en qwiic functionaliteit toegevoegd in flask_prototype optional motor_control toegevoegd knoppen toegevoegd aan website template

--- Python/motor_control.py
+++ Python/motor_control.py
@@ -1,18 +1,19 @@
 from gpiozero import LED
+import time
 
 brug_open_pin = LED(17)
 brug_sluit_pin = LED(18)
 
-def brug_open():
+def brug_open(delay = None):
     print("brug opening...")  # temp testing
     brug_open_pin.on()
+    if delay:
+        time.sleep(delay)
     brug_open_pin.off()
 
-def brug_sluit():
+def brug_sluit(delay = None):
     print("brug closing...")  # temp testing
     brug_sluit_pin.on()
+    if delay:
+        time.sleep(delay)
     brug_sluit_pin.off()
-
-# temp for testing
-def calibrate():
-    print("calibrating...")
