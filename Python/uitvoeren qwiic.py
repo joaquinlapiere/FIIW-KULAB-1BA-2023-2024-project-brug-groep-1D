93a7d27407b30dac4316e2fac4c311e061705ea8
joaquinlapiere <80705785+joaquinlapiere@users.noreply.github.com>
2024-05-21 20:11:09 +0200

    uitvoeren template aangepast

--- Python/uitvoeren qwiic.py
+++ Python/uitvoeren qwiic.py
@@ -1,3 +1,5 @@
 import qwiic
 
-meeting = qwiic.meeting(100)  #argument in functie = aantal meetingen in lijst
+scale = qwiic.start_scale()
+calibration_factor, nul_gewicht = qwiic.calibrate_scale(scale)
+meeting = qwiic.meeting(scale, calibration_factor, nul_gewicht, 100)  #argument in functie = aantal meetingen in lijst
