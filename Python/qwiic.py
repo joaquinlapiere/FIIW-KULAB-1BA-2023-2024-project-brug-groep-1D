9b3b4fe1e67a41d83d6539b0d3def7a00be04ddc
joaquinlapiere <80705785+joaquinlapiere@users.noreply.github.com>
2024-05-22 02:44:11 +0200

    prints non-actief gezet

--- Python/qwiic.py
+++ Python/qwiic.py
@@ -70,13 +70,13 @@ def meeting(scale, calibration_factor, nul_gewicht, meeting, time_stamp_list, aa
 
     for i in range(0, aantal_meetingen):
         gewicht = read_weight(scale, calibration_factor, nul_gewicht)
-        print(f"Weight: {gewicht:.2f} gram")
+        #print(f"Weight: {gewicht:.2f} gram")
         if reading_interval:
             time.sleep(reading_interval)
         meeting[4*aantal_meetingen+i] = gewicht
         time_stamp_list[4*aantal_meetingen+i] = time.strftime('%H:%M:%S')
-    print(meeting)
-    print(time_stamp_list)
+    #print(meeting)
+    #print(time_stamp_list)
     return meeting, time_stamp_list
 
 #while True:
