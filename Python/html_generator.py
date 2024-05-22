ae5acf9ffad1fb37705637dc5254a953422c86db
Arthur Smalle <arthur@smalle.be>
2024-05-22 02:36:22 +0200

    scale en positie zijn dynamisch html template aangepast

--- Python/html_generator.py
+++ Python/html_generator.py
@@ -1,13 +1,14 @@
 # functie want moet uitgevoerd worden in de main functie
-def generate_html(value_position: int = 13, xdata :list = None, ydata :list = None, debug :bool = False):  # value position indicates the position the values have in the html template
-    template = open("../web/data display website (template).html", "r", newline="\n")
-    generated_site = open("../web/generated_site.html", "w")
+def generate_html(value_position: int = 13, scale_position :int =31, xdata :list = None, ydata :list = None, xscale :int = 0, yscale :int = 300, debug :bool = False):  # value position indicates the position the values have in the html template
+    template = open("web/data display website (template).html", "r", newline="\n")
+    generated_site = open("web/generated_site.html", "w")
 
     # convert data from the files to lists
     template_lines = template.readlines()
     
-    # convert template lines list to 2 string
+    # convert template lines list to 3 string
     template_lines_first_half :str = ""
+    template_lines_middel :str = ""
     template_lines_last_half :str = ""
 
     list_to_string_counter :int = 0
@@ -16,8 +17,13 @@ def generate_html(value_position: int = 13, xdata :list = None, ydata :list = No
         if list_to_string_counter < value_position:
             template_lines_first_half += i
             list_to_string_counter += 1
+        
+        elif list_to_string_counter < scale_position:
+            template_lines_middel += i
+            list_to_string_counter += 1
+        
         else:
-            template_lines_last_half += i
+            template_lines_last_half += i 
 
     if xdata == None:
     # convert raw sensordata to js list
@@ -40,9 +46,11 @@ def generate_html(value_position: int = 13, xdata :list = None, ydata :list = No
     # write rest of the site
     generated_site.writelines(template_lines[value_position + 1:])
 
+    # write scale string
+    timestamp :str = "yAxes: [{ticks: {min: " + str(xscale) + ",max:" + str(yscale) +"}}],"
     # generate string of the site
 
-    generated_site_string :str = template_lines_first_half + x_values + y_values + template_lines_last_half
+    generated_site_string :str = template_lines_first_half + x_values + y_values + template_lines_middel + timestamp + template_lines_last_half
 
     # print debug vars
     if debug:
@@ -62,4 +70,4 @@ def debugprint(var1=None, var2=None, var3=None):
         print("[type] y values: ", type(var3))
         print(var3)
 
-print(generate_html(13, [10,11,12], [1,2,3]))
+print(generate_html(13,31, [10,11,12], [1,2,3], 10, 10, 10))
