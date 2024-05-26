83224b4f7185089919aa8952cd8ad9159f7a4a98
joaquinlapiere <80705785+joaquinlapiere@users.noreply.github.com>
2024-05-26 12:18:35 +0200

    schrijffout in "meting" verbeterd.

--- Python/uitvoeren qwiic.py
+++ Python/uitvoeren qwiic.py
@@ -1,11 +1,11 @@
 import qwiic
 
-aantal_meetingen = 11
+aantal_metingen = 5
 
 scale = qwiic.start_scale()
 calibration_factor, nul_gewicht = qwiic.calibrate_scale(scale)
-meeting = qwiic.start_lijst(aantal_meetingen)
-time_stamp_list = qwiic.start_lijst(aantal_meetingen)
+meetingen = qwiic.start_lijst(aantal_metingen)
+time_stamp_list = qwiic.start_lijst(aantal_metingen)
 
 for i in range(0, 6):
-    meeting, time_stamp_list = qwiic.meeting(scale, calibration_factor, nul_gewicht, meeting, time_stamp_list, aantal_meetingen)
+    meting, time_stamp_list = qwiic.meting(scale, calibration_factor, nul_gewicht, meetingen, time_stamp_list, aantal_metingen)
